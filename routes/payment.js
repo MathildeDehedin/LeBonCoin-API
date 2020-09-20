301e9ecd31a3e45b9d198769ec455f02d73c7764
Mathilde Dehedin <mathilde@macbook-pro.home>
2020-09-20 15:16:43 +0200

    update payment

--- routes/payment.js
+++ routes/payment.js
@@ -6,14 +6,12 @@ router.use(formidableMiddleware());
 
 const stripe = createStripe(process.env.STRIPE_API_SECRET);
 
-// on réceptionne le token
 router.post("/payment", async (req, res) => {
   try {
-    // on envoie le token a Stripe avec le montant
     let { status } = await stripe.charges.create({
       amount: req.fields.amount,
       currency: "eur",
-      description: "Paiement leboncoin pour : " + req.fields.title,
+      description: `Paiement leboncoin pour : ${req.fields.title}`,
       source: req.fields.token,
     });
     res.json({ status });
